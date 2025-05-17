47590314f5959325693b168949b900645cc972d6
vladislav-cherashev <cherashev.vlad@gmail.com>
2025-05-17 11:47:22 +0300

    4

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class AuthService {
+
+  http = inject( HttpClient );
+
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
+
+  login( payload: { username: string, password: string } ) {
+    const fd = new FormData();
+    fd.append( 'username', payload.username );
+    fd.append( 'password', payload.password );
+    return this.http.post( `${ this.baseApiUrl }token`, fd );
+  }
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { log } from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
+import { delay, from, map, skip, take, tap } from 'rxjs';
 
 @Component( {
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   standalone: true,
 
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
-})
+} )
 export class LoginPageComponent {
+
+  authService = inject( AuthService );
+
+  form = new FormGroup( {
+    username: new FormControl<string | null>( null, Validators.required ),
+    password: new FormControl<string | null>( null, Validators.required )
+  } );
+
+  constructor() {
+    from( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ] )
+      .pipe(
+        map( value => value * 2 ),
+        tap( value => {
+          this.form.patchValue( { username: value.toString() } );
+        } )
+      ).subscribe( value => console.log( value ) );
+  }
+
+  onSubmit( event: Event ) {
+    if ( this.form.valid ) {
+      // @ts-ignore
+      this.authService.login( this.form.value ).subscribe( res => {
+        console.log( res );
+      } )
+    }
+  }
 
 }
